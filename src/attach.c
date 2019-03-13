5b5a1275cd427b3e64c5355bd57092ff05dad0eb
liujinye <liujinye@xiaomi.com>
2019-03-13 15:29:14 +0800

    Remove rows from the ephemeral table used by window functions once they are no longer required.
    FossilOrigin-Name: 6ad553192051eaa0c6d929baacde2de07b93c6d09de861028bbce55a2c9bfdd3

--- src/attach.c
+++ src/attach.c
@@ -234,9 +234,7 @@ static void attachFunc(
     sqlite3BtreeEnterAll(db);
     db->init.iDb = 0;
     db->mDbFlags &= ~(DBFLAG_SchemaKnownOk);
-    if( !REOPEN_AS_MEMDB(db) ){
-      rc = sqlite3Init(db, &zErrDyn);
-    }
+    rc = sqlite3Init(db, &zErrDyn);
     sqlite3BtreeLeaveAll(db);
     assert( zErrDyn==0 || rc!=SQLITE_OK );
   }
