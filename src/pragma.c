3352bbece5c8b6e39a8c067dc0c07d778a2cf445
liujinye <liujinye@xiaomi.com>
2019-08-17 15:47:32 +0800

    Merge trunk changes into this branch.
    FossilOrigin-Name: db1e60800bc260cdcd604739daaba72c6b486158123fc62a3898aca4ead33cd3

--- src/pragma.c
+++ src/pragma.c
@@ -1245,7 +1245,7 @@ void sqlite3Pragma(
   }
   break;
 
-#ifndef SQLITE_OMIT_INTROSPECTION_PRAGMAS
+#ifdef SQLITE_INTROSPECTION_PRAGMAS
   case PragTyp_FUNCTION_LIST: {
     int i;
     HashElem *j;
