81c0820ac197697ebda6118f55f7d09add1b7a1e
openvela-robot <openvela-robot@xiaomi.com>
2019-10-25 14:46:05 +0800

    Performance optimization in sqlite3BtreeCursor().
    FossilOrigin-Name: ea068b099c96b8b9526114732d2a6be186cf381b7329d102778ad25b95510c9e

--- src/vdbe.c
+++ src/vdbe.c
@@ -3375,6 +3375,7 @@ case OP_AutoCommit: {
       p->rc = rc = SQLITE_BUSY;
       goto vdbe_return;
     }
+    assert( db->nStatement==0 );
     sqlite3CloseSavepoints(db);
     if( p->rc==SQLITE_OK ){
       rc = SQLITE_DONE;
