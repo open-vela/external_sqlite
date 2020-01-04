127f286527680f3638d8d8498f5642db01d06884
liujinye <liujinye@xiaomi.com>
2020-01-04 16:55:57 +0800

    Fix a problem where the loop for the RHS of a LEFT JOIN uses values from an IN() clause as the second or subsequent field of an index.
    FossilOrigin-Name: 95ef68966c50f311830cba8c9257a4085c93011d205e0e31867c2917fa62a48e

--- src/pragma.c
+++ src/pragma.c
@@ -296,46 +296,6 @@ static const PragmaName *pragmaLocate(const char *zName){
 }
 
 /*
-** Create zero or more entries in the output for the SQL functions
-** defined by FuncDef p.
-*/
-static void pragmaFunclistLine(Vdbe *v, FuncDef *p, int isBuiltin){
-  for(; p; p=p->pNext){
-    const char *zType;
-    const char *zEnc;
-    static const u32 mask = 
-        SQLITE_DETERMINISTIC |
-        SQLITE_DIRECTONLY |
-        SQLITE_SUBTYPE |
-        SQLITE_INNOCUOUS
-    ;
-       
-    if( p->xSFunc==0 ) continue;
-    if( p->xValue!=0 ){
-      zType = "w";
-    }else if( p->xFinalize!=0 ){
-      zType = "a";
-    }else{
-      zType = "s";
-    }
-    if( p->funcFlags & SQLITE_UTF8 ){
-      zEnc = "utf8";
-    }else if( p->funcFlags & SQLITE_UTF16BE ){
-      zEnc = "utf16be";
-    }else{
-      zEnc = "utf16le";
-    }
-    sqlite3VdbeMultiLoad(v, 1, "sissii",
-       p->zName, isBuiltin,
-       zType, zEnc,
-       p->nArg,
-       (p->funcFlags & mask) ^ SQLITE_INNOCUOUS
-    );
-  }
-}
-
-
-/*
 ** Helper subroutine for PRAGMA integrity_check:
 **
 ** Generate code to output a single-column result row with a value of the
@@ -1299,16 +1259,16 @@ void sqlite3Pragma(
     int i;
     HashElem *j;
     FuncDef *p;
-    pParse->nMem = 6;
+    pParse->nMem = 2;
     for(i=0; i<SQLITE_FUNC_HASH_SZ; i++){
       for(p=sqlite3BuiltinFunctions.a[i]; p; p=p->u.pHash ){
         if( p->funcFlags & SQLITE_FUNC_INTERNAL ) continue;
-        pragmaFunclistLine(v, p, 1);
+        sqlite3VdbeMultiLoad(v, 1, "si", p->zName, 1);
       }
     }
     for(j=sqliteHashFirst(&db->aFunc); j; j=sqliteHashNext(j)){
       p = (FuncDef*)sqliteHashData(j);
-      pragmaFunclistLine(v, p, 0);
+      sqlite3VdbeMultiLoad(v, 1, "si", p->zName, 0);
     }
   }
   break;
