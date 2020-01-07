4bd61df81a94052d2acfc82bdef0a9a46895d2dc
liujinye <liujinye@xiaomi.com>
2020-01-07 18:10:56 +0800

    Enforce SQLITE_VTABRISK restrictions.
    FossilOrigin-Name: 3d87ff312e617a26846d482b423163cad9c222513f33e128f0fe348dda27c7c9

--- src/btree.c
+++ src/btree.c
@@ -2138,11 +2138,11 @@ static MemPage *btreePageLookup(BtShared *pBt, Pgno pgno){
 */
 static Pgno btreePagecount(BtShared *pBt){
   assert( (pBt->nPage & 0x80000000)==0 || CORRUPT_DB );
-  return pBt->nPage;
+  return pBt->nPage & 0x7fffffff;
 }
 u32 sqlite3BtreeLastPage(Btree *p){
   assert( sqlite3BtreeHoldsMutex(p) );
-  return btreePagecount(p->pBt) & 0x7fffffff;
+  return btreePagecount(p->pBt);
 }
 
 /*
