c3a577cc571e5c77f597fda89879433070aea1af
liujinye <liujinye@xiaomi.com>
2020-01-08 00:39:37 +0800

    Fix a misworded comment.  No code changes.
    FossilOrigin-Name: ee0bc7ede0fbf3e54bb934c0d580c62d7393c984cbc374ece6d57fe9211dac26

--- tool/mkpragmatab.tcl
+++ tool/mkpragmatab.tcl
@@ -128,11 +128,6 @@ set pragma_def {
   ARG:  SQLITE_RecTriggers
   IF:   !defined(SQLITE_OMIT_FLAG_PRAGMAS)
 
-  NAME: trusted_schema
-  TYPE: FLAG
-  ARG:  SQLITE_TrustedSchema
-  IF:   !defined(SQLITE_OMIT_FLAG_PRAGMAS)
-
   NAME: foreign_keys
   TYPE: FLAG
   ARG:  SQLITE_ForeignKeys
@@ -262,7 +257,7 @@ set pragma_def {
 
   NAME: function_list
   FLAG: Result0
-  COLS: name builtin type enc narg flags
+  COLS: name builtin
   IF:   !defined(SQLITE_OMIT_SCHEMA_PRAGMAS)
   IF:   !defined(SQLITE_OMIT_INTROSPECTION_PRAGMAS)
 
