b59be9297b811e13ddbb62c343672d795d3bffc8
openvela-robot <openvela-robot@xiaomi.com>
2020-10-01 17:24:36 +0800

    Add experimental "trigram" tokenizer to fts5.
    FossilOrigin-Name: c4e8ec7907de7cb9e610ef9f4930caf5c046618151fb4f61d5716b9fa3f31423

--- src/wherecode.c
+++ src/wherecode.c
@@ -608,7 +608,12 @@ static int codeEqualityTerm(
           pIn++;
         }
       }
-      if( iEq>0 && (pLoop->wsFlags & WHERE_IN_SEEKSCAN)==0 ){
+      testcase( iEq>0
+                && (pLoop->wsFlags & WHERE_IN_SEEKSCAN)==0
+                && (pLoop->wsFlags & WHERE_VIRTUALTABLE)!=0 );
+      if( iEq>0 
+       && (pLoop->wsFlags & (WHERE_IN_SEEKSCAN|WHERE_VIRTUALTABLE))==0
+      ){
         sqlite3VdbeAddOp3(v, OP_SeekHit, pLevel->iIdxCur, 0, iEq);
       }
     }else{
