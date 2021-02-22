3858d3e545863406d42f331a11ed2965ea519ec3
liujinye <liujinye@xiaomi.com>
2021-02-22 15:44:45 +0800

    When a sub-transaction is released, if no pages required by containing sub-transactions were journaled, truncate the statement journal. This might prevent out-of-control statement journal growth in some cases.
    FossilOrigin-Name: e36327fb22db08763a82fb517407ff5ab0dbc053953098033e7e50796a777810

--- src/where.c
+++ src/where.c
@@ -2956,7 +2956,7 @@ static int whereLoopAddBtree(
 
   if( pSrc->fg.isIndexedBy ){
     /* An INDEXED BY clause specifies a particular index to use */
-    pProbe = pSrc->u2.pIBIndex;
+    pProbe = pSrc->pIBIndex;
   }else if( !HasRowid(pTab) ){
     pProbe = pTab->pIndex;
   }else{
