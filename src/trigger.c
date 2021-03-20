106c59f7c5c9e92a402863d383d9be0ab94a8fed
openvela-robot <openvela-robot@xiaomi.com>
2021-03-20 23:15:52 +0800

    Fix the "box" output mode in the shell when statement returns zero-column rows (for example from "PRAGMA incremental_vacuum").
    FossilOrigin-Name: 34439fe3aeea7cbbc817245d39c345a7f5df7a82ac15ee4d71bb9a4d818198ed

--- src/trigger.c
+++ src/trigger.c
@@ -209,12 +209,12 @@ void sqlite3BeginTrigger(
   */
   if( pTab->pSelect && tr_tm!=TK_INSTEAD ){
     sqlite3ErrorMsg(pParse, "cannot create %s trigger on view: %S", 
-        (tr_tm == TK_BEFORE)?"BEFORE":"AFTER", pTableName->a);
+        (tr_tm == TK_BEFORE)?"BEFORE":"AFTER", pTableName, 0);
     goto trigger_orphan_error;
   }
   if( !pTab->pSelect && tr_tm==TK_INSTEAD ){
     sqlite3ErrorMsg(pParse, "cannot create INSTEAD OF"
-        " trigger on table: %S", pTableName->a);
+        " trigger on table: %S", pTableName, 0);
     goto trigger_orphan_error;
   }
 
@@ -611,7 +611,7 @@ void sqlite3DropTrigger(Parse *pParse, SrcList *pName, int noErr){
   }
   if( !pTrigger ){
     if( !noErr ){
-      sqlite3ErrorMsg(pParse, "no such trigger: %S", pName->a);
+      sqlite3ErrorMsg(pParse, "no such trigger: %S", pName, 0);
     }else{
       sqlite3CodeVerifyNamedSchema(pParse, zDb);
     }
