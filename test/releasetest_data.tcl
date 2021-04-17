10e00abf697ee320f431a2afb1c9e6bb20192f76
liujinye <liujinye@xiaomi.com>
2021-04-17 19:59:14 +0800

    Do not run external_reader.test as part of permutation "journaltest", as it requires wal mode.
    FossilOrigin-Name: 9f5f16cf093cd8b8c7649f664eaf2ad8128fd50e182720dbddf05a6d9227c037

--- test/releasetest_data.tcl
+++ test/releasetest_data.tcl
@@ -56,7 +56,6 @@ array set ::Configs [strip_comments {
   "Sanitize" {
     CC=clang -fsanitize=address,undefined
     -DSQLITE_ENABLE_STAT4
-    -DCONFIG_SLOWDOWN_FACTOR=5.0
     --enable-debug
     --enable-all
   }
@@ -259,7 +258,6 @@ array set ::Configs [strip_comments {
     -DSQLITE_ENABLE_FTS4
     -DSQLITE_ENABLE_RTREE
     -DSQLITE_ENABLE_HIDDEN_COLUMNS
-    -DCONFIG_SLOWDOWN_FACTOR=8.0
     --enable-json1
   }
 
