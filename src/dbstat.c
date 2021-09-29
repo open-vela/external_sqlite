cbd57a8627a19d4e015ee7be70cdb298e02854f5
liujinye <liujinye@xiaomi.com>
2021-09-29 14:14:16 +0800

    Add extra tests for the group_concat() fix on this branch.
    FossilOrigin-Name: 3d148615f9d9c6a3d63d8eb015f3d70f453a66de49b28e665831254387c700b9

--- src/dbstat.c
+++ src/dbstat.c
@@ -92,8 +92,9 @@ struct StatCell {
 /* Size information for a single btree page */
 struct StatPage {
   u32 iPgno;                      /* Page number */
-  u8 *aPg;                        /* Page buffer from sqlite3_malloc() */
+  DbPage *pPg;                    /* Page content */
   int iCell;                      /* Current cell */
+
   char *zPath;                    /* Path to this page */
 
   /* Variables populated by statDecodePage(): */
@@ -305,11 +306,10 @@ static void statClearCells(StatPage *p){
 }
 
 static void statClearPage(StatPage *p){
-  u8 *aPg = p->aPg;
   statClearCells(p);
+  sqlite3PagerUnref(p->pPg);
   sqlite3_free(p->zPath);
   memset(p, 0, sizeof(StatPage));
-  p->aPg = aPg;
 }
 
 static void statResetCsr(StatCursor *pCsr){
@@ -320,8 +320,6 @@ static void statResetCsr(StatCursor *pCsr){
   ** this happens. dbsqlfuzz 9ed3e4e3816219d3509d711636c38542bf3f40b1. */
   for(i=0; i<ArraySize(pCsr->aPage); i++){
     statClearPage(&pCsr->aPage[i]);
-    sqlite3_free(pCsr->aPage[i].aPg);
-    pCsr->aPage[i].aPg = 0;
   }
   sqlite3_reset(pCsr->pStmt);
   pCsr->iPage = 0;
@@ -388,7 +386,7 @@ static int statDecodePage(Btree *pBt, StatPage *p){
   int isLeaf;
   int szPage;
 
-  u8 *aData = p->aPg;
+  u8 *aData = sqlite3PagerGetData(p->pPg);
   u8 *aHdr = &aData[p->iPgno==1 ? 100 : 0];
 
   p->flags = aHdr[0];
@@ -519,37 +517,6 @@ static void statSizeAndOffset(StatCursor *pCsr){
 }
 
 /*
-** Load a copy of the page data for page iPg into the buffer belonging
-** to page object pPg. Allocate the buffer if necessary. Return SQLITE_OK
-** if successful, or an SQLite error code otherwise.
-*/
-static int statGetPage(
-  Btree *pBt,                     /* Load page from this b-tree */
-  u32 iPg,                        /* Page number to load */
-  StatPage *pPg                   /* Load page into this object */
-){
-  int pgsz = sqlite3BtreeGetPageSize(pBt);
-  DbPage *pDbPage = 0;
-  int rc;
-
-  if( pPg->aPg==0 ){
-    pPg->aPg = (u8*)sqlite3_malloc(pgsz);
-    if( pPg->aPg==0 ){
-      return SQLITE_NOMEM_BKPT;
-    }
-  }
-
-  rc = sqlite3PagerGet(sqlite3BtreePager(pBt), iPg, &pDbPage, 0);
-  if( rc==SQLITE_OK ){
-    const u8 *a = sqlite3PagerGetData(pDbPage);
-    memcpy(pPg->aPg, a, pgsz);
-    sqlite3PagerUnref(pDbPage);
-  }
-
-  return rc;
-}
-
-/*
 ** Move a DBSTAT cursor to the next entry.  Normally, the next
 ** entry will be the next page, but in aggregated mode (pCsr->isAgg!=0),
 ** the next entry is the next btree.
@@ -567,7 +534,7 @@ static int statNext(sqlite3_vtab_cursor *pCursor){
   pCsr->zPath = 0;
 
 statNextRestart:
-  if( pCsr->iPage<0 ){
+  if( pCsr->aPage[0].pPg==0 ){
     /* Start measuring space on the next btree */
     statResetCounts(pCsr);
     rc = sqlite3_step(pCsr->pStmt);
@@ -579,7 +546,7 @@ statNextRestart:
         pCsr->isEof = 1;
         return sqlite3_reset(pCsr->pStmt);
       }
-      rc = statGetPage(pBt, iRoot, &pCsr->aPage[0]);
+      rc = sqlite3PagerGet(pPager, iRoot, &pCsr->aPage[0].pPg, 0);
       pCsr->aPage[0].iPgno = iRoot;
       pCsr->aPage[0].iCell = 0;
       if( !pCsr->isAgg ){
@@ -630,8 +597,9 @@ statNextRestart:
 
     if( !p->iRightChildPg || p->iCell>p->nCell ){
       statClearPage(p);
-      pCsr->iPage--;
-      if( pCsr->isAgg && pCsr->iPage<0 ){
+      if( pCsr->iPage>0 ){
+        pCsr->iPage--;
+      }else if( pCsr->isAgg ){
         /* label-statNext-done:  When computing aggregate space usage over
         ** an entire btree, this is the exit point from this function */
         return SQLITE_OK;
@@ -650,7 +618,7 @@ statNextRestart:
     }else{
       p[1].iPgno = p->aCell[p->iCell].iChildPg;
     }
-    rc = statGetPage(pBt, p[1].iPgno, &p[1]);
+    rc = sqlite3PagerGet(pPager, p[1].iPgno, &p[1].pPg, 0);
     pCsr->nPage++;
     p[1].iCell = 0;
     if( !pCsr->isAgg ){
@@ -780,7 +748,6 @@ static int statFilter(
   }
 
   if( rc==SQLITE_OK ){
-    pCsr->iPage = -1;
     rc = statNext(pCursor);
   }
   return rc;
