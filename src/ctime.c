a8bc6c393cc5f2a86346b86a755782de3fe46be0
liujinye <liujinye@xiaomi.com>
2022-01-06 17:13:56 +0800

    Add the '-guard:cf' compiler option for Windows 10, per [forum:/forumpost/8d3b4ad694|forum post 8d3b4ad694].
    FossilOrigin-Name: 2d6a16caa7d28ad5c766036b2eb6c2020683fcc9389b3c7df2013739929dd36f

--- src/ctime.c
+++ src/ctime.c
@@ -179,11 +179,6 @@ static const char * const sqlite3azCompileOpt[] = {
 #ifdef SQLITE_DISABLE_INTRINSIC
   "DISABLE_INTRINSIC",
 #endif
-#ifdef SQLITE_DISABLE_JSON
-  "DISABLE_JSON",
-#else
-  "ENABLE_JSON1",  /* Legacy */
-#endif
 #ifdef SQLITE_DISABLE_LFS
   "DISABLE_LFS",
 #endif
@@ -261,6 +256,9 @@ static const char * const sqlite3azCompileOpt[] = {
 #endif
 #ifdef SQLITE_ENABLE_IOTRACE
   "ENABLE_IOTRACE",
+#endif
+#ifdef SQLITE_ENABLE_JSON1
+  "ENABLE_JSON1",
 #endif
 #ifdef SQLITE_ENABLE_LOAD_EXTENSION
   "ENABLE_LOAD_EXTENSION",
