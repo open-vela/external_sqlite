b5359e0118ac2896a4bea0f0186b89fbbbb457b4
openvela-robot <openvela-robot@xiaomi.com>
2022-06-25 19:30:07 +0800

    Increase the version number to 3.40.0 so as to begin the next development cycle.
    FossilOrigin-Name: 4542e32302067f24bf71c77620e09663f47c3ddce703c21ffa82d09532ae1d51

--- ext/fiddle/testing1.js
+++ ext/fiddle/testing1.js
@@ -16,7 +16,6 @@
 (function(){
     const T = self.SqliteTestUtil;
     const log = console.log.bind(console);
-    const debug = console.debug.bind(console);
 
     const assert = function(condition, text) {
         if (!condition) {
@@ -27,13 +26,9 @@
     const test1 = function(db,sqlite3){
         const api = sqlite3.api;
         log("Basic sanity tests...");
-        T.assert(db._pDb).
-            assert(0===api.sqlite3_extended_result_codes(db._pDb,1));
-        let st = db.prepare(
-            new TextEncoder('utf-8').encode("select 3 as a")
-            /* Testing handling of Uint8Array input */
-        );
-        //debug("statement =",st);
+        T.assert(db._pDb);
+        let st = db.prepare("select 3 as a");
+        //log("statement =",st);
         T.assert(st._pStmt)
             .assert(!st._mayGet)
             .assert('a' === st.getColumnName(0))
@@ -52,7 +47,6 @@
             .assert(3.0 === st.get(0,api.SQLITE_FLOAT))
             .assert(3.0 === st.getFloat(0))
             .assert(st.get(0,api.SQLITE_BLOB) instanceof Uint8Array)
-            .assert(1===st.get(0,api.SQLITE_BLOB).length)
             .assert(st.getBlob(0) instanceof Uint8Array)
             .assert(3 === st.get([])[0])
             .assert(3 === st.get({}).a)
@@ -68,49 +62,39 @@
 
         let list = [];
         db.exec({
-            sql:['CREATE TABLE t(a,b);',
-                 "INSERT INTO t(a,b) VALUES(1,2),(3,4),",
-                 "(?,?),('blob',X'6869');"
-                ],
+            sql:`CREATE TABLE t(a,b);
+INSERT INTO t(a,b) VALUES(1,2),(3,4),(?,?);`,
             multi: true,
             saveSql: list,
             bind: [5,6]
-            /* Achtung: ^^^ bind support might be removed from multi-mode exec. */
         });
         T.assert(2 === list.length);
-        //debug("Exec'd SQL:", list);
-
-        let blob = db.selectValue("select b from t where a='blob'");
-        T.assert(blob instanceof Uint8Array).
-            assert(0x68===blob[0] && 0x69===blob[1]);
-        blob = null;
-
+        //log("Exec'd SQL:", list);
         let counter = 0, colNames = [];
         list.length = 0;
-        db.exec(new TextEncoder('utf-8').encode("SELECT a a, b b FROM t"),{
+        db.exec("SELECT a a, b b FROM t",{
             rowMode: 'object',
             resultRows: list,
             columnNames: colNames,
             callback: function(row,stmt){
                 ++counter;
-                T.assert((row.a%2 && row.a<6) || 'blob'===row.a);
+                T.assert(row.a%2 && row.a<6);
             }
         });
         T.assert(2 === colNames.length)
             .assert('a' === colNames[0])
-            .assert(4 === counter)
-            .assert(4 === list.length);
+            .assert(3 === counter)
+            .assert(3 === list.length);
         list.length = 0;
         db.exec("SELECT a a, b b FROM t",{
             rowMode: 'array',
             callback: function(row,stmt){
                 ++counter;
                 T.assert(Array.isArray(row))
-                    .assert((0===row[1]%2 && row[1]<7)
-                            || (row[1] instanceof Uint8Array));
+                    .assert(0===row[1]%2 && row[1]<7);
             }
         });
-        T.assert(8 === counter);
+        T.assert(6 === counter);
     };
 
     const testUDF = function(db){
@@ -118,7 +102,7 @@
         db.createFunction("foo",function(a,b){return a+b});
         T.assert(7===db.selectValue("select foo(3,4)")).
             assert(5===db.selectValue("select foo(3,?)",2)).
-            assert(5===db.selectValue("select foo(?,?2)",[1,4])).
+            assert(5===db.selectValue("select foo(?,?)",[1,4])).
             assert(5===db.selectValue("select foo($a,$b)",{$a:0,$b:5}));
         db.createFunction("bar", {
             arity: -1,
@@ -127,20 +111,15 @@
                 for(let i = 0; i < arguments.length; ++i) rc += arguments[i];
                 return rc;
             }
-        }).createFunction({
-            name: "asis",
-            callback: (arg)=>arg
         });
 
         log("Testing DB::selectValue() w/ UDF...");
         T.assert(0===db.selectValue("select bar()")).
             assert(1===db.selectValue("select bar(1)")).
             assert(3===db.selectValue("select bar(1,2)")).
-            assert(-1===db.selectValue("select bar(1,2,-4)")).
-            assert('hi'===db.selectValue("select asis('hi')"));
-                    
+            assert(-1===db.selectValue("select bar(1,2,-4)"));
+
         const eqApprox = function(v1,v2,factor=0.05){
-            //debug('eqApprox',v1, v2);
             return v1>=(v2-factor) && v1<=(v2+factor);
         };
         
@@ -149,30 +128,8 @@
             assert(null === db.selectValue("select ?",null)).
             assert(null === db.selectValue("select ?",[null])).
             assert(null === db.selectValue("select $a",{$a:null})).
-            assert(eqApprox(3.1,db.selectValue("select 3.0 + 0.1"))).
-            assert(eqApprox(1.3,db.selectValue("select asis(1 + 0.3)")))
+            assert(eqApprox(3.1,db.selectValue("select 3.0 + 0.1")))
         ;
-
-        log("Testing binding and UDF propagation of blobs...");
-        let blobArg = new Uint8Array(2);
-        blobArg.set([0x68, 0x69], 0);
-        let blobRc = db.selectValue("select asis(?1)", blobArg);
-        T.assert(blobRc instanceof Uint8Array).
-            assert(2 === blobRc.length).
-            assert(0x68==blobRc[0] && 0x69==blobRc[1]);
-        blobRc = db.selectValue("select asis(X'6869')");
-        T.assert(blobRc instanceof Uint8Array).
-            assert(2 === blobRc.length).
-            assert(0x68==blobRc[0] && 0x69==blobRc[1]);
-
-        blobArg = new Int8Array(2);
-        blobArg.set([0x68, 0x69]);
-        //debug("blobArg=",blobArg);
-        blobRc = db.selectValue("select asis(?1)", blobArg);
-        T.assert(blobRc instanceof Uint8Array).
-            assert(2 === blobRc.length);
-        //debug("blobRc=",blobRc);
-        T.assert(0x68==blobRc[0] && 0x69==blobRc[1]);
     };
 
     const testAttach = function(db){
@@ -191,28 +148,16 @@
     };
 
     const runTests = function(Module){
+        T.assert(Module._free instanceof Function).
+            assert(Module.allocate instanceof Function).
+            assert(Module.addFunction instanceof Function).
+            assert(Module.removeFunction instanceof Function);
         const sqlite3 = Module.sqlite3;
         const api = sqlite3.api;
         const oo = sqlite3.SQLite3;
-        log("Loaded module:",api.sqlite3_libversion(),
+        console.log("Loaded module:",api.sqlite3_libversion(),
                     api.sqlite3_sourceid());
         log("Build options:",oo.compileOptionUsed());
-        log("api.wasm.HEAP8 size =",api.wasm.HEAP8().length);
-        log("wasmEnum",JSON.parse(Module.ccall('sqlite3_wasm_enum_json', 'string', [])));
-        [ /* Spot-check a handful of constants to make sure they got installed... */
-            'SQLITE_SCHEMA','SQLITE_NULL','SQLITE_UTF8',
-            'SQLITE_STATIC', 'SQLITE_DIRECTONLY',
-            'SQLITE_OPEN_CREATE'
-        ].forEach(function(k){
-            T.assert('number' === typeof api[k]);
-        });
-        [/* Spot-check a few of the WASM API methods. */
-            '_free', '_malloc', 'addFunction', 'stackRestore'
-        ].forEach(function(k){
-            T.assert(Module[k] instanceof Function).
-                assert(api.wasm[k] instanceof Function);
-        });
-
         const db = new oo.DB();
         try {
             log("DB:",db.filename);
@@ -229,10 +174,11 @@
         log("Total Test count:",T.counter);
     };
 
-    sqlite3InitModule(self.sqlite3TestModule).then(function(theModule){
-        /** Use a timeout so that we are (hopefully) out from under
-            the module init stack when our setup gets run. Just on
-            principle, not because we _need_ to be. */
+    initSqlite3Module(self.sqlite3TestModule).then(function(theModule){
+        /** Use a timeout so that we are (hopefully) out from
+            under the module init stack when our setup gets
+            run. Just on principle, not because we _need_ to
+            be. */
         //console.debug("theModule =",theModule);
         setTimeout(()=>runTests(theModule), 0);
     });
