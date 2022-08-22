2c729468850d318bd495809f1e3a9ce2c25185cb
openvela-robot <openvela-robot@xiaomi.com>
2022-08-22 21:37:17 +0800

    More experimentation with how client-side config state can be passed on to initialize the JS-side sqlite3 API.
    FossilOrigin-Name: ea2acc454c012a62556f6d0623d6eff60736d24aa214a64462b423623ef44d47

--- ext/wasm/api/sqlite3-api-cleanup.js
+++ ext/wasm/api/sqlite3-api-cleanup.js
@@ -11,34 +11,60 @@
   ***********************************************************************
 
   This file is the tail end of the sqlite3-api.js constellation,
-  intended to be appended after all other files so that it can clean
-  up any global systems temporarily used for setting up the API's
-  various subsystems.
+  intended to be appended after all other sqlite3-api-*.js files so
+  that it can finalize any setup and clean up any global symbols
+  temporarily used for setting up the API's various subsystems.
 */
 'use strict';
-self.sqlite3.postInit.forEach(
-  self.importScripts/*global is a Worker*/
-    ? function(f){
-      /** We try/catch/report for the sake of failures which happen in
-          a Worker, as those exceptions can otherwise get completely
-          swallowed, leading to confusing downstream errors which have
-          nothing to do with this failure. */
-      try{ f(self, self.sqlite3) }
-      catch(e){
-        console.error("Error in postInit() function:",e);
-        throw e;
+if('undefined' !== typeof Module){ // presumably an Emscripten build
+  /**
+     Replace sqlite3ApiBootstrap() with a variant which plugs in the
+     Emscripten-based config for all config options which the client
+     does not provide.
+  */
+  const SAB = self.sqlite3ApiBootstrap;
+  self.sqlite3ApiBootstrap = function(apiConfig){
+    apiConfig = apiConfig || {};
+    const configDefaults = {
+      Module: Module /* ==> Emscripten-style Module object. Currently
+                        needs to be exposed here for test code. NOT part
+                        of the public API. */,
+      exports: Module['asm'],
+      memory: Module.wasmMemory /* gets set if built with -sIMPORT_MEMORY */
+    };
+    const config = {};
+    Object.keys(configDefaults).forEach(function(k){
+      config[k] = Object.getOwnPropertyDescriptor(apiConfig, k)
+        ? apiConfig[k] : configDefaults[k];
+    });
+    // Copy over any properties apiConfig defines but configDefaults does not...
+    Object.keys(apiConfig).forEach(function(k){
+      if(!Object.getOwnPropertyDescriptor(config, k)){
+        config[k] = apiConfig[k];
       }
-    }
-  : (f)=>f(self, self.sqlite3)
-);
-delete self.sqlite3.postInit;
-if(self.location && +self.location.port > 1024){
-  console.warn("Installing sqlite3 bits as global S for dev-testing purposes.");
-  self.S = self.sqlite3;
+    });
+    return SAB(config);
+  };
+
+  /**
+     For current (2022-08-22) purposes, automatically call
+     sqlite3ApiBootstrap().  That decision will be revisited at some
+     point, as we really want client code to be able to call this to
+     configure certain parts. If the global sqliteApiConfig property
+     is available, it is assumed to be a config object for
+     sqlite3ApiBootstrap().
+  */
+  //console.warn("self.sqlite3ApiConfig = ",self.sqlite3ApiConfig);
+  const sqlite3 = self.sqlite3ApiBootstrap(self.sqlite3ApiConfig || Object.create(null));
+  delete self.sqlite3ApiBootstrap;
+
+  if(self.location && +self.location.port > 1024){
+    console.warn("Installing sqlite3 bits as global S for dev-testing purposes.");
+    self.S = sqlite3;
+  }
+
+  /* Clean up temporary references to our APIs... */
+  delete sqlite3.capi.util /* arguable, but these are (currently) internal-use APIs */;
+  //console.warn("Module.sqlite3 =",Module.sqlite3);
+  Module.sqlite3 = sqlite3 /* Currently needed by test code and sqlite3-worker1.js */;
 }
-/* Clean up temporary global-scope references to our APIs... */
-self.sqlite3.config.Module.sqlite3 = self.sqlite3
-/* ^^^^ Currently needed by test code and Worker API setup */;
-delete self.sqlite3.capi.util /* arguable, but these are (currently) internal-use APIs */;
-delete self.sqlite3 /* clean up our global-scope reference */;
-//console.warn("Module.sqlite3 =",Module.sqlite3);
