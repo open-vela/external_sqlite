62fce5c4afd14e46917d6b86070f60d4949de1ee
liujinye <liujinye@xiaomi.com>
2022-08-30 00:11:51 +0800

    Any function call can abort.  Take this into account when deciding if a DML statement needs a statement journal.  See [forum:/forumpost/9b9e4716c0d7bbd1|forum thread 9b9e4716c0d7bbd1] for more information.
    FossilOrigin-Name: 5c95ae6c9b93b9bcf698bb1cad93b2da2e28121b35e7c539b1ddc0ef2de33cfe

--- ext/wasm/common/whwasmutil.js
+++ ext/wasm/common/whwasmutil.js
@@ -654,15 +654,6 @@ self.WhWasmUtilInstaller = function(target){
     toss('Invalid type for setMemValue(): ' + type);
   };
 
-
-  /** Convenience form of getMemValue() intended for fetching
-      pointer-to-pointer values. */
-  target.getPtrValue = (ptr)=>target.getMemValue(ptr, ptrIR);
-
-  /** Convenience form of setMemValue() intended for setting
-      pointer-to-pointer values. */
-  target.setPtrValue = (ptr, value)=>target.setMemValue(ptr, value, ptrIR);
-
   /**
      Expects ptr to be a pointer into the WASM heap memory which
      refers to a NUL-terminated C-style string encoded as UTF-8.
@@ -678,18 +669,6 @@ self.WhWasmUtilInstaller = function(target){
     return pos - ptr;
   };
 
-  /** Internal helper to use in operations which need to distinguish
-      between SharedArrayBuffer heap memory and non-shared heap. */
-  const __SAB = ('undefined'===typeof SharedArrayBuffer)
-        ? function(){} : SharedArrayBuffer;
-  const __utf8Decode = function(arrayBuffer, begin, end){
-    return cache.utf8Decoder.decode(
-      (arrayBuffer.buffer instanceof __SAB)
-        ? arrayBuffer.slice(begin, end)
-        : arrayBuffer.subarray(begin, end)
-    );
-  };
-
   /**
      Expects ptr to be a pointer into the WASM heap memory which
      refers to a NUL-terminated C-style string encoded as UTF-8. This
@@ -699,7 +678,11 @@ self.WhWasmUtilInstaller = function(target){
   */
   target.cstringToJs = function(ptr){
     const n = this.cstrlen(ptr);
-    return n ? __utf8Decode(heapWrappers().HEAP8U, ptr, ptr+n) : (null===n ? n : "");
+    if(null===n) return n;
+    return n
+      ? cache.utf8Decoder.decode(
+        new Uint8Array(heapWrappers().HEAP8U.buffer, ptr, n)
+      ) : "";
   }.bind(target);
 
   /**
@@ -1087,11 +1070,11 @@ self.WhWasmUtilInstaller = function(target){
   
   /**
      Looks up a WASM-exported function named fname from
-     target.exports. If found, it is called, passed all remaining
+     target.exports.  If found, it is called, passed all remaining
      arguments, and its return value is returned to xCall's caller. If
      not found, an exception is thrown. This function does no
-     conversion of argument or return types, but see xWrap() and
-     xCallWrapped() for variants which do.
+     conversion of argument or return types, but see xWrap()
+     and xCallWrapped() for variants which do.
 
      As a special case, if passed only 1 argument after the name and
      that argument in an Array, that array's entries become the
