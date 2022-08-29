9cb9340f9dfabde120e322fb1626a8917dcd6f8a
openvela-robot <openvela-robot@xiaomi.com>
2022-08-29 08:04:55 +0800

    oo #1 api: correct a case where a null callback is called. Rename some vars for clarity. Increase wasm-side memory in order to be able to load the speedtest1 output.
    FossilOrigin-Name: b5058f14fadbc8a1886f27cff08593dd2c8e2b2cb6d7bed3b8733a55f031989f

--- src/btree.c
+++ src/btree.c
@@ -8350,8 +8350,7 @@ static int balance_nonroot(
     aPgOrder[iBest] = 0xffffffff;
     if( iBest!=i ){
       if( iBest>i ){
-        sqlite3PagerRekey(apNew[iBest]->pDbPage, pBt->nPage+iBest+1,
-                          aPgFlags[iBest]);
+        sqlite3PagerRekey(apNew[iBest]->pDbPage, pBt->nPage+iBest+1, 0);
       }
       sqlite3PagerRekey(apNew[i]->pDbPage, pgno, aPgFlags[iBest]);
       apNew[i]->pgno = pgno;
