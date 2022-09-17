96483e27aab6b760e2fa581fe646fe967bb60e8c
openvela-robot <openvela-robot@xiaomi.com>
2022-09-17 20:50:12 +0800

    Add the remaining vfs/io_methods wrappers to the OPFS sync/async proxy, but most are not yet tested.
    FossilOrigin-Name: 44db9132145b3072488ea91db53f6c06be74544beccad5fd07efd22c0f03dc04

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -5456,8 +5456,4 @@ void sqlite3VectorErrorMsg(Parse*, Expr*);
 const char **sqlite3CompileOptions(int *pnOpt);
 #endif
 
-#if SQLITE_OS_UNIX && defined(SQLITE_OS_KV_OPTIONAL)
-int sqlite3KvvfsInit(void);
-#endif
-
 #endif /* SQLITEINT_H */
