318339975b77c8902f3d90515c2e845f2dcbf9a3
openvela-robot <openvela-robot@xiaomi.com>
2022-09-30 14:35:18 +0800

    Improvements to the "demoDelete" method in the demovfs, suggested by [forum:/forumpost/cad18961c2|forum post cad18961c2].
    FossilOrigin-Name: d76a301e22e0572262917dfe9557a5051339a65416a473dd2d9989a49975403e

--- src/main.c
+++ src/main.c
@@ -3352,19 +3352,6 @@ static int openDatabase(
     goto opendb_out;
   }
 
-#if SQLITE_OS_UNIX && defined(SQLITE_OS_KV_OPTIONAL)
-  /* Process magic filenames ":localStorage:" and ":sessionStorage:" */
-  if( zFilename && zFilename[0]==':' ){
-    if( strcmp(zFilename, ":localStorage:")==0 ){
-      zFilename = "file:local?vfs=kvvfs";
-      flags |= SQLITE_OPEN_URI;
-    }else if( strcmp(zFilename, ":sessionStorage:")==0 ){
-      zFilename = "file:session?vfs=kvvfs";
-      flags |= SQLITE_OPEN_URI;
-    }
-  }
-#endif /* SQLITE_OS_UNIX && defined(SQLITE_OS_KV_OPTIONAL) */
-
   /* Parse the filename/URI argument
   **
   ** Only allow sensible combinations of bits in the flags argument.  
@@ -4794,8 +4781,8 @@ int sqlite3_snapshot_open(
 */
 int sqlite3_snapshot_recover(sqlite3 *db, const char *zDb){
   int rc = SQLITE_ERROR;
-#ifndef SQLITE_OMIT_WAL
   int iDb;
+#ifndef SQLITE_OMIT_WAL
 
 #ifdef SQLITE_ENABLE_API_ARMOR
   if( !sqlite3SafetyCheckOk(db) ){
