6ae5a1f623037a02813253196366e020823a398b
liujinye <liujinye@xiaomi.com>
2022-10-01 13:17:53 +0800

    Replace the Parse.disableVtab field with Parse.prepFlags for increased generality, a small size reduction, and a small performance increase.
    FossilOrigin-Name: b7da0bcdf70e53ab1ec00a0694e17c7429e23bc6eb3f39b622d06a930aa2f6a3

--- ext/wasm/testing1.js
+++ ext/wasm/testing1.js
@@ -19,8 +19,7 @@
   const toss = function(...args){throw new Error(args.join(' '))};
   const debug = console.debug.bind(console);
   const eOutput = document.querySelector('#test-output');
-  const log = console.log.bind(console),
-        warn = console.warn.bind(console);
+  const log = console.log.bind(console)
   const logHtml = function(...args){
     log.apply(this, args);
     const ln = document.createElement('div');
@@ -33,8 +32,6 @@
     return v1>=(v2-factor) && v1<=(v2+factor);
   };
 
-  let sqlite3 /* loaded later */;
-  
   const testBasicSanity = function(db,sqlite3){
     const capi = sqlite3.capi;
     log("Basic sanity tests...");
@@ -165,10 +162,10 @@
     }
 
     try {
-      throw new sqlite3.WasmAllocError;
+      throw new capi.WasmAllocError;
     }catch(e){
       T.assert(e instanceof Error)
-        .assert(e instanceof sqlite3.WasmAllocError);
+        .assert(e instanceof capi.WasmAllocError);
     }
 
     try {
@@ -186,7 +183,7 @@
     // Custom db error message handling via sqlite3_prepare_v2/v3()
     if(capi.wasm.exports.sqlite3_wasm_db_error){
       log("Testing custom error message via prepare_v3()...");
-      let rc = capi.sqlite3_prepare_v3(db.pointer, {/*invalid*/}, -1, 0, null, null);
+      let rc = capi.sqlite3_prepare_v3(db.pointer, [/*invalid*/], -1, 0, null, null);
       T.assert(capi.SQLITE_MISUSE === rc)
         .assert(0 === capi.sqlite3_errmsg(db.pointer).indexOf("Invalid SQL"));
       log("errmsg =",capi.sqlite3_errmsg(db.pointer));
@@ -253,8 +250,8 @@
     const resultRows = [];
     db.exec({
       sql:new TextEncoder('utf-8').encode([
-        // ^^^ testing string-vs-typedarray handling in exec()
-        "attach 'session' as foo;" /* name 'session' is magic for kvvfs! */,
+        // ^^^ testing string-vs-typedarray handling in execMulti()
+        "attach 'foo.db' as foo;",
         "create table foo.bar(a);",
         "insert into foo.bar(a) values(1),(2),(3);",
         "select a from foo.bar order by a;"
@@ -266,31 +263,11 @@
     T.assert(3===resultRows.length)
       .assert(2===resultRows[1]);
     T.assert(2===db.selectValue('select a from foo.bar where a>1 order by a'));
-    let colCount = 0, rowCount = 0;
-    const execCallback = function(pVoid, nCols, aVals, aNames){
-      colCount = nCols;
-      ++rowCount;
-      T.assert(2===aVals.length)
-        .assert(2===aNames.length)
-        .assert(+(aVals[1]) === 2 * +(aVals[0]));
-    };
-    const capi = sqlite3.capi;
-    let rc = capi.sqlite3_exec(
-      db.pointer, "select a, a*2 from foo.bar", execCallback,
-      0, 0
-    );
-    T.assert(0===rc).assert(3===rowCount).assert(2===colCount);
-    rc = capi.sqlite3_exec(
-      db.pointer, "select a from foo.bar", ()=>{
-        toss("Testing throwing from exec() callback.");
-      }, 0, 0
-    );
-    T.assert(capi.SQLITE_ABORT === rc);
     db.exec("detach foo");
     T.mustThrow(()=>db.exec("select * from foo.bar"));
   };
 
-  const testIntPtr = function(db,S){
+  const testIntPtr = function(db,S,Module){
     const w = S.capi.wasm;
     const stack = w.scopedAllocPush();
     let ptrInt;
@@ -737,14 +714,17 @@
       //log("sfile",sfile,sfile.constructor.prototype);
       T.assert(0===sfile.$pMethods).assert(iom.pointer > 0);
       //log("iom",iom);
+      /** Some of the following tests require that pMethods has a
+          signature of "P", as opposed to "p". */
+      sfile.$pMethods = iom;
+      T.assert(iom === sfile.$pMethods);
       sfile.$pMethods = iom.pointer;
-      T.assert(iom.pointer === sfile.$pMethods)
+      T.assert(iom === sfile.$pMethods)
         .assert(IOM.resolveToInstance(iom))
         .assert(undefined ===IOM.resolveToInstance(sfile))
         .mustThrow(()=>IOM.resolveToInstance(0,true))
         .assert(S3F.resolveToInstance(sfile.pointer))
-        .assert(undefined===S3F.resolveToInstance(iom))
-        .assert(iom===IOM.resolveToInstance(sfile.$pMethods));
+        .assert(undefined===S3F.resolveToInstance(iom));
       T.assert(0===iom.$iVersion);
       installIOMethods(iom);
       T.assert(1===iom.$iVersion);
@@ -763,7 +743,7 @@
         .assert('sqlite3_vfs' === dVfs.structName)
         .assert(!!dVfs.structInfo)
         .assert(SB.StructType.hasExternalPointer(dVfs))
-        .assert(dVfs.$iVersion>0)
+        .assert(3===dVfs.$iVersion)
         .assert('number'===typeof dVfs.$zName)
         .assert('number'===typeof dVfs.$xSleep)
         .assert(capi.wasm.functionEntry(dVfs.$xOpen))
@@ -1024,21 +1004,15 @@
     }
   }/*testWasmUtil()*/;
 
-  const clearKvvfs = function(){
-    const sz = sqlite3.capi.sqlite3_web_kvvfs_size();
-    const n = sqlite3.capi.sqlite3_web_kvvfs_clear('');
-    log("Cleared kvvfs local/sessionStorage:",
-        n,"entries totaling approximately",sz,"bytes.");
-  };
-
-  const runTests = function(_sqlite3){
-    sqlite3 = _sqlite3;
-    const capi = sqlite3.capi,
+  const runTests = function(Module){
+    //log("Module",Module);
+    const sqlite3 = Module.sqlite3,
+          capi = sqlite3.capi,
           oo = sqlite3.oo1,
           wasm = capi.wasm;
     log("Loaded module:",capi.sqlite3_libversion(), capi.sqlite3_sourceid());
     log("Build options:",wasm.compileOptionUsed());
-    capi.sqlite3_wasmfs_opfs_dir()/*will install OPFS if available, plus a and non-locking VFS*/;
+
     if(1){
       /* Let's grab those last few lines of test coverage for
          sqlite3-api.js... */
@@ -1071,19 +1045,9 @@
       T.assert(capi.wasm[k] instanceof Function);
     });
 
-    let dbName = "/testing1.sqlite3";
-    let vfsName = undefined;
-    if(capi.sqlite3_web_db_uses_vfs(0,"kvvfs")){
-      dbName = "local";
-      vfsName = 'kvvfs';
-      logHtml("Found kvvfs. Clearing db(s) from sessionStorage and localStorage",
-              "and selecting kvvfs-friendly db name:",dbName);
-      clearKvvfs();
-    }
-    const db = new oo.DB(dbName,'c',vfsName), startTime = performance.now();
-    log("db is kvvfs?",capi.sqlite3_web_db_uses_vfs(db.pointer,"kvvfs"));
+    const db = new oo.DB(':memory:'), startTime = performance.now();
     try {
-      log("db.filename =",db.filename,"db.fileName() =",db.getFilename());
+      log("DB filename:",db.filename,db.fileName());
       const banner1 = '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',
             banner2 = '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<';
       [
@@ -1093,18 +1057,24 @@
       ].forEach((f)=>{
         const t = T.counter, n = performance.now();
         logHtml(banner1,"Running",f.name+"()...");
-        f(db, sqlite3);
+        f(db, sqlite3, Module);
         logHtml(banner2,f.name+"():",T.counter - t,'tests in',(performance.now() - n),"ms");
       });
     }finally{
       db.close();
-      if('kvvfs'===vfsName) clearKvvfs();
     }
     logHtml("Total Test count:",T.counter,"in",(performance.now() - startTime),"ms");
     log('capi.wasm.exports',capi.wasm.exports);
   };
 
-  self.sqlite3TestModule.initSqlite3().then((S)=>{
-    runTests(S);
+  sqlite3InitModule(self.sqlite3TestModule).then(function(theModule){
+    /** Use a timeout so that we are (hopefully) out from under
+        the module init stack when our setup gets run. Just on
+        principle, not because we _need_ to be. */
+    //console.debug("theModule =",theModule);
+    //setTimeout(()=>runTests(theModule), 0);
+    // ^^^ Chrome warns: "VIOLATION: setTimeout() handler took A WHOLE 50ms!"
+    self._MODULE = theModule /* this is only to facilitate testing from the console */
+    runTests(theModule);
   });
 })();
