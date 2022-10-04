85aa301d7139c7f8c32b652211943d84901b8207
openvela-robot <openvela-robot@xiaomi.com>
2022-10-04 11:14:23 +0800

    Tweaks to the opfs async wait/relinquish times.
    FossilOrigin-Name: 35f33c23e5849de1c43c4499ee0a7fa11d26ae34949c1e820c3fa8e8873f9c2b

--- tool/mkctimec.tcl
+++ tool/mkctimec.tcl
@@ -43,7 +43,7 @@ set ::headCode "
 ** autoconf-based build
 */
 #if defined(_HAVE_SQLITE_CONFIG_H) && !defined(SQLITECONFIG_H)
-#include \"sqlite_cfg.h\"
+#include \"config.h\"
 #define SQLITECONFIG_H 1
 #endif
 
