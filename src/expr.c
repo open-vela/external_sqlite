3da4da8798b490c8d9ada2f058b14ea81f7ffe9d
openvela-robot <openvela-robot@xiaomi.com>
2022-10-15 15:39:46 +0800

    Merge latest trunk changes.
    FossilOrigin-Name: 42255ead8e44b24a8abc04aca49f170c55f3b52f42366a0d8ef248ee65fcc7ce

--- src/expr.c
+++ src/expr.c
@@ -4043,46 +4043,6 @@ static int exprCodeInlineFunction(
   return target;
 }
 
-/*
-** Check to see if pExpr is one of the indexed expression on pParse->pIdxExpr.
-** If it is, then resolve the expression by reading from the index and
-** return the register into which the value has been read.  If there is
-** no match, return negative.
-*/
-static SQLITE_NOINLINE int sqlite3ExprIndexLookup(
-  Parse *pParse,   /* The parsing context */
-  Expr *pExpr,     /* The expression to potentially bypass */
-  int target       /* Where to store the result of the expression */
-){
-  IndexExpr *p;
-  Vdbe *v;
-  for(p=pParse->pIdxExpr; p; p=p->pIENext){
-    if( p->iDataCur<0 ) continue;
-    if( sqlite3ExprCompare(0, pExpr, p->pExpr, p->iDataCur)!=0 ) continue;
-    v = pParse->pVdbe;
-    assert( v!=0 );
-    if( p->bMaybeNullRow ){
-      /* If the index is on a NULL row due to an outer join, then we
-      ** cannot extract the value from the index.  The value must be
-      ** computed using the original expression. */
-      int addr = sqlite3VdbeCurrentAddr(v);
-      sqlite3VdbeAddOp3(v, OP_IfNullRow, p->iIdxCur, addr+3, target);
-      VdbeCoverage(v);
-      sqlite3VdbeAddOp3(v, OP_Column, p->iIdxCur, p->iIdxCol, target);
-      sqlite3VdbeGoto(v, 0);
-      p = pParse->pIdxExpr;
-      pParse->pIdxExpr = 0;
-      sqlite3ExprCode(pParse, pExpr, target);
-      pParse->pIdxExpr = p;
-      sqlite3VdbeJumpHere(v, addr+2);
-    }else{
-      sqlite3VdbeAddOp3(v, OP_Column, p->iIdxCur, p->iIdxCol, target);
-    }
-    return target;
-  }
-  return -1;  /* Not found */
-}
-
 
 /*
 ** Generate code into the current Vdbe to evaluate the given
@@ -4107,14 +4067,6 @@ int sqlite3ExprCodeTarget(Parse *pParse, Expr *pExpr, int target){
 
   assert( target>0 && target<=pParse->nMem );
   assert( v!=0 );
-
-  if( pParse->pIdxExpr!=0 
-   && pExpr!=0
-   && !ExprHasProperty(pExpr, EP_Leaf)
-   && (r1 = sqlite3ExprIndexLookup(pParse, pExpr, target))>=0
-  ){
-    return r1;
-  }
 
 expr_code_doover:
   if( pExpr==0 ){
@@ -5618,13 +5570,7 @@ int sqlite3ExprCompare(
     if( pB->op==TK_COLLATE && sqlite3ExprCompare(pParse, pA,pB->pLeft,iTab)<2 ){
       return 1;
     }
-    if( pA->op==TK_AGG_COLUMN && pB->op==TK_COLUMN 
-     && pB->iTable<0 && pA->iTable==iTab
-    ){
-      /* fall through */
-    }else{
-      return 2;
-    }
+    return 2;
   }
   assert( !ExprHasProperty(pA, EP_IntValue) );
   assert( !ExprHasProperty(pB, EP_IntValue) );
