793a002b6a3e60c445e7a7835aeb1f980c463f53
openvela-robot <openvela-robot@xiaomi.com>
2022-10-16 15:38:03 +0800

    Add a top-level license and build-time version info header to generated sqlite3*.js. Correct a broken link in ext/wasm/index.html.
    FossilOrigin-Name: 0f1a06e8e39a1fbc74f1aff9cc59787282dfbf847d6c5c7edb3f7d410db0e4b7

--- main.mk
+++ main.mk
@@ -446,9 +446,6 @@ TESTSRC2 = \
   $(TOP)/ext/misc/stmt.c \
   $(TOP)/ext/session/sqlite3session.c \
   $(TOP)/ext/session/test_session.c \
-  $(TOP)/ext/recover/sqlite3recover.c \
-  $(TOP)/ext/misc/dbdata.c \
-  $(TOP)/ext/recover/test_recover.c \
   fts5.c
 
 # Header files used by all library source files.
@@ -765,8 +762,6 @@ SHELL_SRC = \
 	$(TOP)/ext/misc/zipfile.c \
 	$(TOP)/ext/misc/memtrace.c \
 	$(TOP)/ext/misc/dbdata.c \
-	$(TOP)/ext/recover/sqlite3recover.c \
-	$(TOP)/ext/recover/sqlite3recover.h \
         $(TOP)/src/test_windirent.c
 
 shell.c:	$(SHELL_SRC) $(TOP)/tool/mkshellc.tcl
