e06b8f6ce3c411f5e491ded795673e57275eae49
liujinye <liujinye@xiaomi.com>
2022-11-17 19:24:39 +0800

    Fix corner cases in UTF8 handling in the REGEXP extension. [forum:/forumpost/3ffe058b04|Forum post 3ffe058b04].
    FossilOrigin-Name: abb18f61c5cec0f524acc41453b4c06b61c5af51ff46417588837fc0c3967288

--- ext/wasm/api/sqlite3-api-opfs.js
+++ ext/wasm/api/sqlite3-api-opfs.js
@@ -166,12 +166,7 @@ const installOpfsVfs = function callee(options){
       opfsVfs.dispose();
       return promiseReject_(err);
     };
-    const W =
-#ifdef SQLITE_JS_ESM
-    new Worker(new URL(options.proxyUri, import.meta.url));
-#else
-    new Worker(options.proxyUri);
-#endif
+    const W = new Worker(options.proxyUri);
     W._originalOnError = W.onerror /* will be restored later */;
     W.onerror = function(err){
       // The error object doesn't contain any useful info when the
@@ -574,7 +569,7 @@ const installOpfsVfs = function callee(options){
         const ndx = Math.random() * (f._n * 64) % f._n | 0;
         a[i] = f._chars[ndx];
       }
-      return a.join("");
+      return a.join('');
     };
 
     /**
@@ -1163,7 +1158,7 @@ const installOpfsVfs = function callee(options){
             be set here.
           */
           //"pragma cache_size=-8388608;"
-        ].join("")
+        ].join('')
       );
     }
 
