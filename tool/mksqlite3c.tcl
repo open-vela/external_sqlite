7adbf13e9514055e745051fee58d910584f779d9
openvela-robot <openvela-robot@xiaomi.com>
2022-11-24 02:59:33 +0800

    Speed up base64 conversions, and add test with more data for the baseNN conversion to grind.
    FossilOrigin-Name: 6c84ae4ba83713c751fddff8be41686bbcb525ac8135e1520436c62d0bc23d2c

--- tool/mksqlite3c.tcl
+++ tool/mksqlite3c.tcl
@@ -40,14 +40,11 @@ set help {Usage: tclsh mksqlite3c.tcl <options>
 set addstatic 1
 set linemacros 0
 set useapicall 0
-set enable_recover 0
 set srcdir tsrc
 
 for {set i 0} {$i<[llength $argv]} {incr i} {
   set x [lindex $argv $i]
-  if {[regexp {^-?-enable-recover$} $x]} {
-    set enable_recover 1
-  } elseif {[regexp {^-?-nostatic$} $x]} {
+  if {[regexp {^-?-nostatic$} $x]} {
     set addstatic 0
   } elseif {[regexp {^-?-linemacros(?:=([01]))?$} $x ma ulm]} {
     if {$ulm == ""} {set ulm 1}
@@ -81,9 +78,7 @@ close $in
 # Open the output file and write a header comment at the beginning
 # of the file.
 #
-set fname sqlite3.c
-if {$enable_recover} { set fname sqlite3r.c }
-set out [open $fname w]
+set out [open sqlite3.c w]
 # Force the output to use unix line endings, even on Windows.
 fconfigure $out -translation lf
 set today [clock format [clock seconds] -format "%Y-%m-%d %H:%M:%S UTC" -gmt 1]
@@ -167,7 +162,6 @@ foreach hdr {
    vxworks.h
    wal.h
    whereInt.h
-   sqlite3recover.h
 } {
   set available_hdr($hdr) 1
 }
@@ -331,7 +325,7 @@ proc copy_file {filename} {
 # used subroutines first in order to help the compiler find
 # inlining opportunities.
 #
-set flist {
+foreach file {
    sqliteInt.h
    os_common.h
    ctime.c
@@ -447,11 +441,7 @@ set flist {
    sqlite3session.c
    fts5.c
    stmt.c
-} 
-if {$enable_recover} {
-  lappend flist sqlite3recover.c dbdata.c
-}
-foreach file $flist {
+} {
   copy_file $srcdir/$file
 }
 
