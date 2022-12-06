675d6c71282ace6ec6c6583ceb79819b9d240ed3
liujinye <liujinye@xiaomi.com>
2022-12-07 03:42:39 +0800

    Add addOnDispose() method to Jaccwabyt and code-adjacent minor internal cleanups.
    FossilOrigin-Name: 6a2723fe3f28dd94328d901e64e1e9ee9a1b2e9eeaed6c54038a5b83c914db78

--- src/vdbeapi.c
+++ src/vdbeapi.c
@@ -15,7 +15,6 @@
 */
 #include "sqliteInt.h"
 #include "vdbeInt.h"
-#include "opcodes.h"
 
 #ifndef SQLITE_OMIT_DEPRECATED
 /*
@@ -2226,7 +2225,10 @@ int sqlite3_stmt_scanstatus_v2(
             for(iOp=0; iOp<p->nOp; iOp++){
               Op *pOp = &p->aOp[iOp];
               if( pOp->p1!=iEnd ) continue;
-              if( (sqlite3OpcodeProperty[pOp->opcode] & OPFLG_NCYCLE)==0 ){
+              if( pOp->opcode!=OP_VFilter && pOp->opcode!=OP_VColumn
+               && pOp->opcode!=OP_Rowid   && pOp->opcode!=OP_VOpen
+               && pOp->opcode!=OP_VNext
+              ){
                 continue;
               }
               res += p->anCycle[iOp];
