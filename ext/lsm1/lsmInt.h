8dbac3d6c156bb41f35971bbd450d14e8612c61a
openvela-robot <openvela-robot@xiaomi.com>
2023-01-10 14:31:56 +0800

    Fix handling of unix paths that contain ".." components such that "/" is considered its own parent directory.
    FossilOrigin-Name: 3c6cadb396de3981bd950eddd532daa8134bd4bf22c578620e323835c96a8500

--- ext/lsm1/lsmInt.h
+++ ext/lsm1/lsmInt.h
@@ -405,7 +405,7 @@ struct Segment {
   LsmPgno iFirst;                  /* First page of this run */
   LsmPgno iLastPg;                 /* Last page of this run */
   LsmPgno iRoot;                   /* Root page number (if any) */
-  LsmPgno nSize;                   /* Size of this run in pages */
+  int nSize;                       /* Size of this run in pages */
 
   Redirect *pRedirect;             /* Block redirects (or NULL) */
 };
@@ -852,8 +852,6 @@ int lsmVarintPut32(u8 *, int);
 int lsmVarintGet32(u8 *, int *);
 int lsmVarintPut64(u8 *aData, i64 iVal);
 int lsmVarintGet64(const u8 *aData, i64 *piVal);
-
-int lsmVarintLen64(i64);
 
 int lsmVarintLen32(int);
 int lsmVarintSize(u8 c);
