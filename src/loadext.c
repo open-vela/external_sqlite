22c7a36cd4389d80fb95868a8802aec51695114c
openvela-robot <openvela-robot@xiaomi.com>
2023-01-11 16:02:20 +0800

    Fix another 64-bit offset problem in lsm.
    FossilOrigin-Name: 1f3d0bdc20aa74a595f7bb7b2c152259a9d3a8ffbe7cc229c57ad2142df5376d

--- src/loadext.c
+++ src/loadext.c
@@ -510,9 +510,7 @@ static const sqlite3_api_routines sqlite3Apis = {
 #endif
   sqlite3_db_name,
   /* Version 3.40.0 and later */
-  sqlite3_value_encoding,
-  /* Version 3.41.0 and later */
-  sqlite3_is_interrupted
+  sqlite3_value_encoding
 };
 
 /* True if x is the directory separator character
