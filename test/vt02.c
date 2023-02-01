88127e59c48ba468c094846d59ff0bf57563f201
openvela-robot <openvela-robot@xiaomi.com>
2023-02-01 20:45:12 +0800

    New assert() statements to verify that sqlite3DeleteIndexSamples() is always called with non-NULL parameters.
    FossilOrigin-Name: 92c71fdd7167a7db055ef0aec522bbba8c7b4b37658efda10dac44699921b57f

--- test/vt02.c
+++ test/vt02.c
@@ -177,8 +177,6 @@
   #include <assert.h>
 #endif
 
-#ifndef SQLITE_OMIT_VIRTUALTABLE
-
 /* Forward declarations */
 typedef struct vt02_vtab vt02_vtab;
 typedef struct vt02_cur vt02_cur;
@@ -1000,10 +998,6 @@ static void vt02CoreInit(sqlite3 *db){
   sqlite3_create_module(db, "vt02pkx", &vt02Module, (void*)zPkXSchema);
   sqlite3_create_module(db, "vt02pkabcd", &vt02Module, (void*)zPkABCDSchema);
 }
-
-#else
-# define vt02CoreInit(db)
-#endif /* ifndef SQLITE_OMIT_VIRTUALTABLE */
 
 #ifdef TH3_VERSION
 static void vt02_init(th3state *p, int iDb, char *zArg){
