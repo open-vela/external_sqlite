947403720d4bb87f8ea4ea57aca8179eadb9328e
liujinye <liujinye@xiaomi.com>
2023-02-03 19:28:50 +0800

    Fix a problem where optimizing an fts5 table too often might cause it to become unreadable.
    FossilOrigin-Name: 35bed981416269018a0d52171d9997c43d3b097f5db1910d10d882eabe2e137f

--- test/releasetest_data.tcl
+++ test/releasetest_data.tcl
@@ -417,7 +417,7 @@ proc main_script {args} {
     usage
   }
   set config [lindex $args end-1]
-  set srcdir [lindex $args end]
+  set target [lindex $args end]
 
   set opts       [list]                         ;# OPTS value
   set cflags     [expr {$bMsvc ? "-Zi" : "-g"}] ;# CFLAGS value
@@ -542,16 +542,14 @@ proc main_script {args} {
     puts {set -e}
     puts {}
     puts {if [ "$#" -ne 1 ] ; then}
-    puts {  echo "Usage: $0 <target>" }
+    puts {  echo "Usage: $0 <sqlite-src-dir>" }
     puts {  exit -1 }
     puts {fi }
-    puts "SRCDIR=\"$srcdir\""
+    puts {SRCDIR=$1}
     puts {}
     puts "TCL=\"[::tcl::pkgconfig get libdir,install]\""
 
-    puts {if [ ! -f Makefile ] ; then}
-    puts "  \$SRCDIR/configure --with-tcl=\$TCL $configOpts"
-    puts {fi}
+    puts "\$SRCDIR/configure --with-tcl=\$TCL $configOpts"
     puts {}
     puts {OPTS="      -DSQLITE_NO_SYNC=1"}
     foreach o $opts { 
@@ -560,11 +558,11 @@ proc main_script {args} {
     puts {}
     puts "CFLAGS=\"$cflags\""
     puts {}
-    puts "make \$1 \"CFLAGS=\$CFLAGS\" \"OPTS=\$OPTS\" $makeOpts"
+    puts "make $target \"CFLAGS=\$CFLAGS\" \"OPTS=\$OPTS\" $makeOpts"
   } else {
 
-    puts {set TARGET=%1}
-    set makecmd    "nmake /f %SRCDIR%\\Makefile.msc TOP=\"$srcdir\" %TARGET%"
+    puts {set SRCDIR=%1}
+    set makecmd    "nmake /f %SRCDIR%\\Makefile.msc TOP=%SRCDIR% $target "
     append makecmd "\"CFLAGS=$cflags\" \"OPTS=$opts\" $makeOpts"
 
     puts "set TMP=%CD%"
