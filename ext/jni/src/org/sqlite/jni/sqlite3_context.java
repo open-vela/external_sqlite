44e258af240ecb18faa7becdf488588041ecaccd
openvela-robot <openvela-robot@xiaomi.com>
2023-07-28 01:12:47 +0800

    Add support making use of sqlite3_aggregate_context() (in a roundabout way) from Java to accumulate state within aggregate and window UDFs.
    FossilOrigin-Name: 640574984741c7a9472d7f8be7bce87e736d7947ce673ae4a25008d74238ad90

---
+++ ext/jni/src/org/sqlite/jni/sqlite3_context.java
@@ -0,0 +1,54 @@
+/*
+** 2023-07-21
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*************************************************************************
+** This file is part of the JNI bindings for the sqlite3 C API.
+*/
+package org.sqlite.jni;
+
+/**
+   sqlite3_context instances are used in conjunction with user-defined
+   SQL functions (a.k.a. UDFs). They are opaque pointers.
+
+   The getAggregateContext() method corresponds to C's
+   sqlite3_aggregate_context(), with a slightly different interface in
+   order to account for cross-language differences. It serves the same
+   purposes in a slightly different way: it provides a key which is
+   stable across invocations of UDF xStep() and xFinal() pairs, to
+   which a UDF may map state across such calls (e.g. a numeric result
+   which is being accumulated).
+*/
+public class sqlite3_context extends NativePointerHolder<sqlite3_context> {
+  public sqlite3_context() {
+    super();
+  }
+  private long aggcx = 0;
+
+  /**
+     If this object is being used in the context of an aggregate or
+     window UDF, the UDF binding layer will set a unique context value
+     here. That value will be the same across matching calls to the
+     xStep() and xFinal() routines, as well as xValue() and xInverse()
+     in window UDFs. This value can be used as a key to map state
+     which needs to persist across such calls, noting that such state
+     should be cleaned up via xFinal().
+  */
+  public long getAggregateContext(){
+    return aggcx;
+  }
+
+  /**
+     For use only by the JNI layer. It's permitted to call this even
+     though it's private.
+  */
+  private void setAggregateContext(long n){
+    aggcx = n;
+  }
+}
