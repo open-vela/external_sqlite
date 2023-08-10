94ddb33bfca00787992e743cb66fd5cc83a66d71
liujinye <liujinye@xiaomi.com>
2023-08-10 17:39:26 +0800

    Merge trunk into jni branch for the newly-relocated version-info tool.
    FossilOrigin-Name: cc8e8cba67c0dcfb9b416041a19456cf5248d909f3efb6fee707a5950be4f374

---
+++ ext/jni/src/org/sqlite/jni/OutputPointer.java
@@ -0,0 +1,69 @@
+/*
+** 2023-07-21
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*************************************************************************
+** This file is part of the JNI bindings for the sqlite3 C API.
+*/
+package org.sqlite.jni;
+
+/**
+   Helper classes for handling JNI output pointers.
+
+   We do not use a generic OutputPointer<T> because working with those
+   from the native JNI code is unduly quirky due to a lack of
+   autoboxing at that level.
+*/
+public final class OutputPointer {
+  public static final class sqlite3 {
+    private org.sqlite.jni.sqlite3 value;
+    public sqlite3(){value = null;}
+    public void clear(){value = null;}
+    public final org.sqlite.jni.sqlite3 getValue(){return value;}
+  }
+
+  public static final class sqlite3_stmt {
+    private org.sqlite.jni.sqlite3_stmt value;
+    public sqlite3_stmt(){value = null;}
+    public void clear(){value = null;}
+    public final org.sqlite.jni.sqlite3_stmt getValue(){return value;}
+  }
+
+  public static final class Int32 {
+    private int value;
+    public Int32(){this(0);}
+    public Int32(int v){value = v;}
+    public final int getValue(){return value;}
+    public final void setValue(int v){value = v;}
+  }
+
+  public static final class Int64 {
+    private long value;
+    public Int64(){this(0);}
+    public Int64(long v){value = v;}
+    public final long getValue(){return value;}
+    public final void setValue(long v){value = v;}
+  }
+
+  public static final class String {
+    private java.lang.String value;
+    public String(){this(null);}
+    public String(java.lang.String v){value = v;}
+    public final java.lang.String getValue(){return value;}
+    public final void setValue(java.lang.String v){value = v;}
+  }
+
+  public static final class ByteArray {
+    private byte[] value;
+    public ByteArray(){this(null);}
+    public ByteArray(byte[] v){value = v;}
+    public final byte[] getValue(){return value;}
+    public final void setValue(byte[] v){value = v;}
+  }
+}
