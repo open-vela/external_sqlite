27a6baaaa909ce44b770b5eff4820df7b84471fe
openvela-robot <openvela-robot@xiaomi.com>
2023-08-11 20:32:40 +0800

    Fix a makefile deps problem which caused ext/jni build to fail if sqlite3.c/h were not created beforehand.
    FossilOrigin-Name: 101de670774f63757180282763730aa53e70198bd7a674c27e6044632d39d22a

--- src/json.c
+++ src/json.c
@@ -3009,13 +3009,7 @@ static void jsonValidFunc(
 ){
   JsonParse *p;          /* The parse */
   UNUSED_PARAMETER(argc);
-  if( sqlite3_value_type(argv[0])==SQLITE_NULL ){
-#ifdef SQLITE_LEGACY_JSON_VALID
-    /* Incorrect legacy behavior was to return FALSE for a NULL input */
-    sqlite3_result_int(ctx, 0);
-#endif
-    return;
-  }
+  if( sqlite3_value_type(argv[0])==SQLITE_NULL ) return;
   p = jsonParseCached(ctx, argv[0], 0, 0);
   if( p==0 || p->oom ){
     sqlite3_result_error_nomem(ctx);
